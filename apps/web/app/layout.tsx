93f4b5756d3c0c6c2d0e765b46c71c6dec80d78f
chronark <dev@chronark.com>
2023-04-28 22:21:47 +0200

    feat: og image

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -14,38 +14,38 @@ export const metadata: Metadata = {
     template: "%s | Highstorm",
   },
   description: "Open Source Event Monitoring",
-  // openGraph: {
-  //   title: 'Lee Robinson',
-  //   description: 'Developer, writer, and creator.',
-  //   url: 'https://leerob.io',
-  //   siteName: 'Lee Robinson',
-  //   images: [
-  //     {
-  //       url: 'https://leerob.io/og.jpg',
-  //       width: 1920,
-  //       height: 1080,
-  //     },
-  //   ],
-  //   locale: 'en-US',
-  //   type: 'website',
-  // },
-  // robots: {
-  //   index: true,
-  //   follow: true,
-  //   googleBot: {
-  //     index: true,
-  //     follow: true,
-  //     'max-video-preview': -1,
-  //     'max-image-preview': 'large',
-  //     'max-snippet': -1,
-  //   },
-  // },
-  // twitter: {
-  //   title: 'Highstorm',
-  //   card: 'summary_large_image',
-  // },
+  openGraph: {
+    title: "Highstorm",
+    description: "Open Source Event Monitoring",
+    url: "https://highstorm.app",
+    siteName: "highstorm.app",
+    images: [
+      {
+        url: "https://highstorm.app/og.png",
+        width: 2322,
+        height: 1306,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Highstorm",
+    card: "summary_large_image",
+  },
   icons: {
-    shortcut: "/favicon.svg",
+    shortcut: "/favicon.png",
   },
 };
 
