b2ccfa5b9da2e17cd04ecab10b27e9f73dfeac12
chronark <dev@chronark.com>
2023-04-28 22:33:30 +0200

    feat: og image

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -43,6 +43,7 @@ export const metadata: Metadata = {
   twitter: {
     title: "Highstorm",
     card: "summary_large_image",
+    description: "Open Source Event Monitoring",
   },
   icons: {
     shortcut: "/favicon.png",
