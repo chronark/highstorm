e8fb8deb11c22941d22f1d66955814436937f0ed
chronark <dev@chronark.com>
2023-04-28 22:10:44 +0200

    refactor(features-02.tsx): remove Features02 component and its import from landing page

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -1,6 +1,5 @@
 import { Hero } from "@/components/landing/hero";
 import { Features } from "@/components/landing/features";
-import { Features02 } from "@/components/landing/features-02";
 import { Pricing } from "@/components/landing/pricing";
 import { Cta } from "@/components/landing/cta";
 
@@ -9,7 +8,6 @@ export default function Page() {
     <div className="max-w-screen overflow-x-hidden">
       <Hero />
       <Features />
-      <Features02 />
       <Pricing />
       <Cta />
     </div>
